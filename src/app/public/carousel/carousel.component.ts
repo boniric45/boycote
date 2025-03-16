2163244cff44117434b424e2c104d9f8d2f34630
boniface <53636957+boniric45@users.noreply.github.com>
2025-03-16 16:49:46 +0100

    implements carousel 3d

--- src/app/public/carousel/carousel.component.ts
+++ src/app/public/carousel/carousel.component.ts
@@ -1,25 +1,61 @@
-import { AfterViewInit, Component, ElementRef, input, QueryList, signal, ViewChild, ViewChildren } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, Input, input, model, OnInit, Output, QueryList, signal, ViewChild, ViewChildren } from '@angular/core';
 import { NgStyle } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon'
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
 
 @Component({
   selector: 'app-carousel',
-  imports: [NgStyle,FormsModule,MatButtonModule],
+  imports: [NgStyle, FormsModule, MatButtonModule, MatIconModule, MatSelectModule, MatInputModule],
   templateUrl: './carousel.component.html',
   styleUrl: './carousel.component.scss'
 })
 export class CarouselComponent implements AfterViewInit {
+  idcollection = 0;
+  selectedIndex = 0;
+  nameCollection = 0;
+  txtCollection = '';
+  @ViewChild('carousel') carousel!:ElementRef;
+  @ViewChildren('carousel__cell') cells!:QueryList<ElementRef>;
+  cellWidth!:number;
+  cellHeight!:number;
+  isHorizontal:boolean = true;
+  rotateFn = this.isHorizontal ? 'rotateY' : 'rotateX';
+  radius!:number
+  theta!:number;
+
+  collections = [1,2,3,4,5,6,7,8,9];
+
+  mapCollection = new Map<number,string>([
+    [1,"Collection 1"],
+    [2,"Collection 2"],
+    [3,"Collection 3"],
+    [4,"Collection 4"],
+    [5,"Collection 5"],
+    [6,"Collection 6"],
+    [7,"Collection 7"],
+    [8,"Collection 8"],
+    [9,"Collection 9"]
+  ]
+
+
+  );
+idCollection: any;
+
+
 
   getStyle(index:number)
-  {
+  {   
+  
     if (!this.cellCount)
        return null;
     const angle=(index-this.selectedIndex)*2*Math.PI/this.cellCount
-    const scale=((75)+25*Math.cos(angle))/100
+    const scale=((75)+5*Math.cos(angle))/110
 
     return {
-      left:-75+150*Math.sin(angle)+'px',
+      left:-30+150*Math.sin(angle)+'px',
       transform:'scale('+scale+')',
       position:'absolute',
       "z-index":Math.floor(100*scale)
@@ -30,27 +66,41 @@ export class CarouselComponent implements AfterViewInit {
   {
     return this.cells?this.cells.length:0;
   }
-  selectedIndex = 0;
-  @ViewChild('carousel') carousel!:ElementRef
-  @ViewChildren('carousel__cell') cells!:QueryList<ElementRef>
-  cellWidth!:number;
-  cellHeight!:number;
-  isHorizontal:boolean = true;
 
-  rotateFn = this.isHorizontal ? 'rotateY' : 'rotateX';
-  radius!:number
-  theta!:number;
-  // console.log( cellWidth, cellHeight );
+
+  // get name of map with index
+  getNameCollections(){
+
+    
+  this.collections.forEach((c)=>{
+    this.idCollection = c;
+
+    if(c === this.selectedIndex){
+      this.nameCollection = c;
+      this.mapCollection.forEach((keys,value) => {
+        if(value == this.selectedIndex){
+          console.log(this.idCollection);
+          this.txtCollection = keys;
+        }
+      })
+    }
+  });
+  }
+
 
   prev()
   {
     this.selectedIndex--;
-    this.rotateCarousel();
+    this.getNameCollections();
+    this.rotateCarousel();  
   }
+
   next()
   {
     this.selectedIndex++;
-    this.rotateCarousel();
+    this.getNameCollections();
+    this.rotateCarousel();   
+
   }
 
   initCarousel() {
