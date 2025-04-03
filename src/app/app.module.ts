745492add7d9c640ae47d6be8f1a7502b7803390
boniface <53636957+boniric45@users.noreply.github.com>
2025-04-03 21:20:57 +0200

    Update Build

---
+++ src/app/app.module.ts
@@ -0,0 +1,10 @@
+import { BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { NgModule } from '@angular/core'
+
+@NgModule({
+  imports: [BrowserModule],
+  declarations: [],
+  bootstrap: []
+})
+export class AppModule { }
